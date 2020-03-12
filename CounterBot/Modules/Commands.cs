fac463a190bc7c11e94ceaf5418ccf9de93ccfd5
ChaosAnkhseram <62097434+ChaosAnkhseram@users.noreply.github.com>
2020-03-12 11:31:42 +0100

    Add files via upload

---
+++ CounterBot/Modules/Commands.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace CounterBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("nazi")]
+        public async Task Rip()
+        {
+            await ReplyAsync("Das kommt auf den Counter");
+        }
+
+    }
+}
